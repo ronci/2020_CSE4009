4b90b5ead0a5b25b533024ba45c7ef48268cfdd9
2019015414 <2019015414@hanyang.ac.kr>
2020-09-22 03:09:20 +0900

    Week3 : logicalNeg Function check

--- Week3/datalab-handout/bits.c
+++ Week3/datalab-handout/bits.c
@@ -277,7 +277,7 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return (((x ^ (~x + 1)) >> 31) & 1) ^ 1;
+	return ((((x ^ (~x + 1)) >> 31) & ((x >> 31) ^ 1)) & 1) ^ 1;
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
